b626b9aac00587bbf983b8e47f1c8886a5d90833
Rafael Leonardo <82063490+aviiamelia@users.noreply.github.com>
2024-05-19 13:39:58 -0300

    Add project files.

---
+++ src/DevicesMonitoring/Controllers/CreateUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevicesMonitoring.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CreateUser : ControllerBase
+{
+    [HttpGet]
+    public IActionResult create()
+    {
+        return Ok("teste");
+    }
+}
